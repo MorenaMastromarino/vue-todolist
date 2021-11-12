0eb3b6088a349a9aab522aab34d55e58e1a79af3
MorenaMastromarino <morena.mastromarino98@gmail.com>
2021-11-12 17:17:57 +0100

    milestone 2 first commit

---
+++ milestone-2/js/script.js
@@ -0,0 +1,24 @@
+const app = new Vue({
+  el: '#app',
+
+  data: {
+    tasks: [
+      {
+        text: 'Fare la spesa',
+        done: false,
+      },
+      {
+        text: 'Pagare bollette',
+        done: false,
+      },
+      {
+        text: 'Ripetere html/css',
+        done: true,
+      },
+      {
+        text: 'Fare esercizio',
+        done: false,
+      }  
+    ],
+  },
+});
